a25d040169efb2ee16ca42ad01e182e7eb066198
marco.padeiro <2211868@my.ipleiria.pt>
2023-06-13 20:34:07 +0100

    Feature: Really basic cli environment, needs some code optimization and refactoring

---
+++ src/cmd.rs
@@ -0,0 +1,44 @@
+// Purpose: Handle command line arguments
+use crate::errors;
+use crate::commands;
+
+pub struct Config {
+    pub arg: String,
+    pub values: Vec<String>,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() == 0 {
+            return Err("usage");
+        }
+
+        if ! args[0].starts_with("-") {
+            return Err("usage");
+        }
+
+        let arg = args[0].clone();
+        let values: Vec<String> = args[1..].to_vec();
+
+        return Ok(Config { arg, values })
+    }
+}
+
+pub fn handle_args(config: Config) {
+    match config.arg.as_str() {
+        "-S" => 
+            if config.values.len() == 0 {
+                errors::handle_error("no packages specified");
+            } else {
+                commands::handle_install(config.values)
+            }
+        "-Ss" => 
+            if config.values.len() == 0 {
+                errors::handle_error("no packages specified");
+            } else {
+                commands::handle_search(config.values[0].clone())
+            }
+        "-Syu" => commands::handle_update(),
+        _ => errors::handle_error(config.arg.as_str()),
+    }
+}
