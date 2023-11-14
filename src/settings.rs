cecd20bb51774d3a7712953376059ac932049b55
Rodrigo Carreira <rodrigocarreira33@gmail.com>
2023-11-14 18:24:15 +0000

    Fixed Stderr
    When verbose was not quiet there was a weird empty error msg

--- src/settings.rs
+++ src/settings.rs
@@ -29,27 +29,33 @@ impl Settings {
         self.no_confirm
     }
 
+    pub fn get_verbose(&self) -> &str {
+        &self.verbose
+    }
+
     pub fn get_verbose_settings(&self) -> (Stdio, Stdio) {
         match self.verbose.as_str() {
-            "verbose" => {
-                (std::process::Stdio::inherit(), std::process::Stdio::inherit())
-            }
-            "quiet" => {
-                (std::process::Stdio::piped(), std::process::Stdio::piped())
-            }
-            _ => {
-                (std::process::Stdio::piped(), std::process::Stdio::inherit())
-            }
+            "verbose" => (
+                std::process::Stdio::inherit(),
+                std::process::Stdio::inherit(),
+            ),
+            "quiet" => (std::process::Stdio::piped(), std::process::Stdio::piped()),
+            _ => (std::process::Stdio::piped(), std::process::Stdio::inherit()),
         }
     }
 }
 
 pub fn read() -> Settings {
-    let path = format!("{}/{}/{}", home::home_dir().unwrap().display(), DEFAULT_PATH, SETTINGS_FILE);
+    let path = format!(
+        "{}/{}/{}",
+        home::home_dir().unwrap().display(),
+        DEFAULT_PATH,
+        SETTINGS_FILE
+    );
     let config_path = std::path::Path::new(&path);
 
     if let Err(_) = std::fs::metadata(config_path) {
-       return create_default(config_path);
+        return create_default(config_path);
     }
 
     let json = std::fs::read_to_string(config_path).unwrap();
@@ -59,13 +65,17 @@ pub fn read() -> Settings {
 
     println!(
         "{}",
-        theme::colorize(theme::Type::Warning,
-    "Your settings file has been updated to the latest version.")
+        theme::colorize(
+            theme::Type::Warning,
+            "Your settings file has been updated to the latest version."
+        )
     );
     println!(
         "{}",
-        theme::colorize(theme::Type::Warning,
-    "Old settings file has been renamed to settings.json.old.")
+        theme::colorize(
+            theme::Type::Warning,
+            "Old settings file has been renamed to settings.json.old."
+        )
     );
     std::fs::rename(config_path, format!("{}.old", path)).unwrap();
 
@@ -89,5 +99,3 @@ fn create_default(path: &Path) -> Settings {
     std::fs::write(path, json).unwrap();
     settings
 }
-
-
